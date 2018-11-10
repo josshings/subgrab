cede6e7c96fa7408ba3e81eb95675cdd4587350d
josshings <44147561+josshings@users.noreply.github.com>
2018-11-10 02:47:18 -0200

    Update subgrab.sh
    Replaced hardcoded test with actual functionality.

--- subgrab.sh
+++ subgrab.sh
@@ -3,7 +3,7 @@
 echo "Subdomains found:"
 echo "-----------------"
 
-for sub in $(cat subs.txt);do
+for sub in $(curl -s -L $1 | grep href= | cut -d "/" -f 3 | grep $1 | cut -d '"' -f 1 | sort -u);do
 	ip=$(host $sub | grep "has address" | cut -d " " -f 4)
 
 	if [ "${#ip}" -ne "0" ]; then
